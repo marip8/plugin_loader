0e5e77f882d5394e0e7a741f7c854bbb4990f756
Michael Ripperger <michael.ripperger@swri.org>
2022-06-16 10:35:13 -0500

    Added helper macro for doing explicit template instantiation for the plugin loader (#6)

--- test/plugin.cpp
+++ test/plugin.cpp
@@ -23,14 +23,10 @@ namespace plugin_loader
 // Define the section name for loading plugins of base class `Printer`
 // This should match the section name specified in the plugin export macro for this class
 const std::string Printer::section = "printer";
-// Explicit template instantiate the plugin loader functions for the `Printer` class
-template std::vector<std::string> PluginLoader::getAllAvailablePlugins<Printer>() const;
-template Printer::Ptr PluginLoader::createInstance(const std::string&) const;
+INSTANTIATE_PLUGIN_LOADER(Printer)
 
 // Define the section name for loading plugins of base class `Shape`
 // This should match the section name specified in the plugin export macro for this class
 const std::string Shape::section = "shape";
-// Explicit template instantiate the plugin loader functions for the `Shape` class
-template std::vector<std::string> PluginLoader::getAllAvailablePlugins<Shape>() const;
-template Shape::Ptr PluginLoader::createInstance(const std::string&) const;
+INSTANTIATE_PLUGIN_LOADER(Shape)
 }  // namespace plugin_loader
